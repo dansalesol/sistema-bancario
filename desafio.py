5e454bc183306c8beda410a160a5fbaf5bf8f054
dansalesol <dsales@gmail.com>
2024-06-11 14:16:45 -0300

    Adição da função transferência

--- desafio.py
+++ desafio.py
@@ -3,15 +3,19 @@ menu = """
 [d] Depositar
 [s] Sacar
 [e] Extrato
+[t] Transferência
 [q] Sair
 ==================================
 => """
 
 saldo = 0
-limite = 500
+limite_saque = 1000
+limite_transferencia = 800
 extrato = ""
 numero_saques = 0
+numero_transferencias = 0
 LIMITE_SAQUES = 3
+LIMITE_TRANSFERENCIA = 2
 
 while True:
 
@@ -32,7 +36,7 @@ while True:
 
         excedeu_saldo = valor > saldo
 
-        excedeu_limite = valor > limite
+        excedeu_limite = valor > limite_saque
 
         excedeu_saques = numero_saques >= LIMITE_SAQUES
 
@@ -58,6 +62,30 @@ while True:
         print("Não foram realizadas movimentações." if not extrato else extrato)
         print(f"\nSaldo: R$ {saldo:.2f}")
         print("==========================================")
+
+    elif opcao == "t":
+        valor = float(input("Informe o valor a ser transferido: "))
+        numero_conta_transferencia = input("Informe o número da conta a ser transferido: ")
+
+        excedeu_saldo = valor > saldo
+
+        excedeu_limite = valor > limite_transferencia
+
+        excedeu_transferencias = numero_transferencias >= LIMITE_TRANSFERENCIA
+
+        if excedeu_saldo:
+            print("Operação falhou! Você não tem saldo suficiente.")
+
+        elif excedeu_limite:
+            print("Operação falhou! O valor do saque excede o limite.")
+
+        elif excedeu_transferencias:
+            print("Operação falhou! Número máximo de transferências excedido.")
+
+        elif valor > 0:
+            saldo -= valor
+            extrato += f"Transferência: R$ {valor:.2f} para conta {numero_conta_transferencia}\n"
+            numero_transferencias += 1
 
     elif opcao == "q":
         break
